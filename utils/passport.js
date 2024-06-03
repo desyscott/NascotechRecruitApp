39f0a17175e93eda162ebf0b62bb85cc8f652533
desy scott <Mrfallback055@gmail.com>
2024-06-03 12:55:37 +0000

    made some changes on passportjs

--- utils/passport.js
+++ utils/passport.js
@@ -12,7 +12,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID, 
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"/api/auth/google/callback",
+    callbackURL:"https://nascotectrecruitapp-api.onrender.com/api/auth/google/callback",
     scope:["profile","email"]
 },
  async (accessToken, refreshToken,callback, profile, done) => {
